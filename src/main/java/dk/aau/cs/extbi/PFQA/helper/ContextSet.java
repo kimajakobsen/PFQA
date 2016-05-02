634a1f5679d5761b901c7505cb591fdd598e467e
Kim A. Jakobsen <kimajakobsen@gmail.com>
2016-05-02 11:26:16 +0200

    Change internal list to contain strings instead of resources.

--- src/main/java/dk/aau/cs/extbi/PFQA/helper/ContextSet.java
+++ src/main/java/dk/aau/cs/extbi/PFQA/helper/ContextSet.java
@@ -3,17 +3,18 @@ package dk.aau.cs.extbi.PFQA.helper;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.lang3.NotImplementedException;
-import org.apache.jena.rdf.model.Resource;
-
 public class ContextSet {
-	List<Resource> contextValues = new ArrayList<Resource>();
+	List<String> contextValues = new ArrayList<String>();
 
 	public ContextSet intersect(ContextSet contextSetPQ) {
-		throw new NotImplementedException("");
+		//System.out.println("AQ cv: "+ contextValues.size());
+		//System.out.println("PQ cv: "+ contextSetPQ.getValues().size());
+		contextValues.retainAll(contextSetPQ.getValues());
+		//System.out.println("intersect: "+this.getValues().size());
+		return this;
 	}
 
-	public void add(Resource contextValue) {
+	public void add(String contextValue) {
 		contextValues.add(contextValue);
 	}
 
@@ -21,7 +22,7 @@ public class ContextSet {
 		contextValues.addAll(contextSet.getValues());
 	}
 	
-	public List<Resource> getValues() {
+	public List<String> getValues() {
 		return contextValues;
 	}
 
