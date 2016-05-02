04a118f28a722f669d946a7972ee9b7e788b2de0
Kim A. Jakobsen <kimajakobsen@gmail.com>
2016-05-02 11:26:41 +0200

    Impl stub

--- src/main/java/dk/aau/cs/extbi/PFQA/provenanceQeuryExecutor/ProvenanceQueryExecutor.java
+++ src/main/java/dk/aau/cs/extbi/PFQA/provenanceQeuryExecutor/ProvenanceQueryExecutor.java
@@ -1,24 +1,34 @@
 package dk.aau.cs.extbi.PFQA.provenanceQeuryExecutor;
 
+import org.apache.jena.query.Dataset;
 import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ReadWrite;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.tdb.TDBFactory;
 
+import dk.aau.cs.extbi.PFQA.helper.Config;
 import dk.aau.cs.extbi.PFQA.helper.ContextSet;
 
 
 public class ProvenanceQueryExecutor {
 
-	public ProvenanceQueryExecutor() {
-		// TODO Auto-generated constructor stub
-	}
-
 	public ContextSet getContext(Query pq) {
-		// TODO Auto-generated method stub
-		return null;
+		ContextSet contextSet = new ContextSet();
+		Dataset dataset = TDBFactory.createDataset(Config.getDatasetLocation()) ;
+		dataset.begin(ReadWrite.READ) ;
+		QueryExecution qexec = QueryExecutionFactory.create(pq, dataset) ;
+		ResultSet results = qexec.execSelect() ;
+		for ( ; results.hasNext() ; ) {
+			  QuerySolution soln = results.nextSolution() ;
+			  contextSet.add(soln.get("?s").toString());
+			}
+		
+		dataset.commit();
+		dataset.end();
+		return contextSet;
 	}
 
-//	   Dataset dataset = TDBFactory.createDataset("/home/kah/Documents/provenanceGenerator/tdb-test/") ;
-//       QueryExecution qexec = QueryExecutionFactory.create(provenanceQuery, dataset) ;
-//       ResultSet results = qexec.execSelect() ;
-//       System.out.println(ResultSetFormatter.asText(results));
-//       ResultSetFormatter.out(results) ;
 }
