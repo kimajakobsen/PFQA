63872b7f65bd2ba369bb661b647a80e93e6b27fb
Kim A. Jakobsen <kimajakobsen@gmail.com>
2016-05-02 10:33:15 +0200

    Made serilizable

--- src/main/java/dk/aau/cs/extbi/PFQA/provenanceIndex/ProvenanceIndex.java
+++ src/main/java/dk/aau/cs/extbi/PFQA/provenanceIndex/ProvenanceIndex.java
@@ -3,7 +3,9 @@ package dk.aau.cs.extbi.PFQA.provenanceIndex;
 import dk.aau.cs.extbi.PFQA.helper.ContextSet;
 import dk.aau.cs.extbi.PFQA.queryProfile.QueryProfile;
 
-public abstract class ProvenanceIndex {
+public abstract class ProvenanceIndex implements java.io.Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	public abstract ContextSet getContext(QueryProfile qp);
 }
