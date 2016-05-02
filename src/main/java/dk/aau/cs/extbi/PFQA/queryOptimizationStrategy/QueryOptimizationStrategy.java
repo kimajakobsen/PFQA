692b50b2be6d3ed00a2589fb9f3c29939bda8d96
Kim A. Jakobsen <kimajakobsen@gmail.com>
2016-05-02 10:32:46 +0200

    Added internal class contextSet + getters and setters

--- src/main/java/dk/aau/cs/extbi/PFQA/queryOptimizationStrategy/QueryOptimizationStrategy.java
+++ src/main/java/dk/aau/cs/extbi/PFQA/queryOptimizationStrategy/QueryOptimizationStrategy.java
@@ -3,8 +3,21 @@ package dk.aau.cs.extbi.PFQA.queryOptimizationStrategy;
 import org.apache.jena.query.Query;
 import org.apache.jena.query.ResultSet;
 
+import dk.aau.cs.extbi.PFQA.helper.ContextSet;
+
 public abstract class QueryOptimizationStrategy {
 
+	protected ContextSet contextSet;
+
 	public abstract ResultSet execute(Query aq) ;
+	
+	public ContextSet getContextSet() {
+		return contextSet;
+	}
+
+	public void setContextSet(ContextSet contextSet) {
+		this.contextSet = contextSet;
+	}
+
 
 }
