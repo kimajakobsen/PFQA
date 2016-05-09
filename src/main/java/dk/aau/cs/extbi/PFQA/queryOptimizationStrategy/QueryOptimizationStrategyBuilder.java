8acbb0de9b797ba4601de19567e65ed855be2d85
Kim A. Jakobsen <kimajakobsen@gmail.com>
2016-05-09 13:32:54 +0200

    Moved the query builder creation inside the optimization strategy builder

--- src/main/java/dk/aau/cs/extbi/PFQA/queryOptimizationStrategy/QueryOptimizationStrategyBuilder.java
+++ src/main/java/dk/aau/cs/extbi/PFQA/queryOptimizationStrategy/QueryOptimizationStrategyBuilder.java
@@ -1,5 +1,7 @@
 package dk.aau.cs.extbi.PFQA.queryOptimizationStrategy;
 
+import org.apache.jena.query.Query;
+
 import dk.aau.cs.extbi.PFQA.helper.ContextSet;
 import dk.aau.cs.extbi.PFQA.provenanceIndex.ProvenanceIndex;
 import dk.aau.cs.extbi.PFQA.provenanceIndex.ProvenanceIndexBuilder;
@@ -7,12 +9,12 @@ import dk.aau.cs.extbi.PFQA.queryProfile.QueryProfile;
 
 public class QueryOptimizationStrategyBuilder {
 	private String type;
-	private QueryProfile queryProfile;
+	private Query analyticalQuery;
 	private String provenanceIndex;
 	
-	public QueryOptimizationStrategyBuilder(String type, QueryProfile queryProfile, String provenanceIndex) {
+	public QueryOptimizationStrategyBuilder(String type, Query analyticalQuery, String provenanceIndex) {
 		this.type = type;
-		this.queryProfile = queryProfile;
+		this.analyticalQuery = analyticalQuery;
 		this.provenanceIndex = provenanceIndex;
 	}
 	
@@ -20,6 +22,8 @@ public class QueryOptimizationStrategyBuilder {
 		if (type.equals("FullMaterilization")) {
 			ProvenanceIndexBuilder provenanceIndexBuilder = new ProvenanceIndexBuilder(provenanceIndex);
 			ProvenanceIndex pi = provenanceIndexBuilder.build(); 
+			
+			QueryProfile queryProfile = new QueryProfile(analyticalQuery);
 			
 			ContextSet contextSetAP = pi.getContext(queryProfile);
 			ContextSet contextSetMinumum = contextSetAP.intersect(contextSetPQ);
