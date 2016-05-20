4471747450ac51ece4fe8200a6bcc2b946a3043c
Kim A. Jakobsen <kimajakobsen@gmail.com>
2016-05-20 15:07:03 +0200

    New impl of intersect

--- src/main/java/dk/aau/cs/extbi/PFQA/helper/ContextSet.java
+++ src/main/java/dk/aau/cs/extbi/PFQA/helper/ContextSet.java
@@ -1,22 +1,13 @@
 package dk.aau.cs.extbi.PFQA.helper;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import dk.aau.cs.extbi.PFQA.logger.Logger;
 
 public class ContextSet {
 	List<String> contextValues = new ArrayList<String>();
-
-	public ContextSet intersect(ContextSet contextSetPQ) {
-		Logger logger = Logger.getInstance();
-		
-		logger.startIntersectContextSet();
-		contextValues.retainAll(contextSetPQ.getValues());
-		logger.endIntersectContextSet();
-		
-		return this;
-	}
 
 	public void add(String contextValue) {
 		contextValues.add(contextValue);
@@ -28,6 +19,39 @@ public class ContextSet {
 	
 	public List<String> getValues() {
 		return contextValues;
+	}
+
+	public ContextSet intersect(ContextSet contextSetPQ) {
+		Logger logger = Logger.getInstance();
+		
+		logger.startIntersectContextSet();
+		List<String> intersection = new ArrayList<String>();
+		HashMap<String,Boolean> map = new HashMap<String, Boolean>();
+		if (contextValues.size() > contextSetPQ.getValues().size()) {
+			for (String string : contextSetPQ.getValues()) {
+				map.put(string, true);
+			}
+			for (String string : contextValues) {
+				if (map.containsKey(string)) {
+					intersection.add(string);
+				}
+			}
+			
+		} else {
+			for (String string : contextValues) {
+				map.put(string, true);
+			}
+			for (String string : contextSetPQ.getValues()) {
+				if (map.containsKey(string)) {
+					intersection.add(string);
+				}
+			}
+		}
+		
+		contextValues = intersection;
+		logger.endIntersectContextSet();
+		
+		return this;
 	}
 
 }
