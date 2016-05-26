de9f2473ffed6f1ac93a66ae51a99c796ae5c9a3
Kim A. Jakobsen <kimajakobsen@gmail.com>
2016-05-26 16:13:32 +0200

    add startup phase

---
+++ src/main/java/dk/aau/cs/extbi/PFQA/logger/StartupTimes.java
@@ -0,0 +1,53 @@
+package dk.aau.cs.extbi.PFQA.logger;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.AbstractMap.SimpleEntry;
+
+public class StartupTimes {
+
+	private long writeIndexToDiskDuration;
+	private long buildIndexDuration;
+	private LocalDateTime time;
+	private String index;
+	private String dataset;
+
+	public StartupTimes(LocalDateTime time, SimpleEntry<String, String> dataset2, String index) {
+		this.time = time;
+		this.index = index;
+		this.dataset = dataset2.getValue();
+	}
+
+	public void setBuildIndex(long buildIndexDuration) {
+		this.buildIndexDuration = (buildIndexDuration);
+	}
+	
+	public void setWriteIndexToDisk(long writeIndexToDiskDuration) {
+        this.writeIndexToDiskDuration = (writeIndexToDiskDuration);
+    }
+	
+	public long getBuildIndexDuration() {
+        return buildIndexDuration;
+    }
+
+    public long getWriteIndexToDiskDuration() {
+        return writeIndexToDiskDuration;
+    }
+    
+    public long getUnixTimestamp() {
+		ZoneId zoneId = ZoneId.systemDefault(); // or: ZoneId.of("Europe/Oslo");
+		return time.atZone(zoneId).toEpochSecond();
+	}
+    
+    public String getIndex() {
+    	return index;
+    }
+
+	public String getDataset() {
+		return dataset;
+	}
+
+	public void setDataset(String dataset) {
+		this.dataset = dataset;
+	}
+}
