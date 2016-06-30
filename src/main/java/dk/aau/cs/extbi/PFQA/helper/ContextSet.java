c488a47f5db886b9565e889900f7c5a27e4907dd
Kim A. Jakobsen <kimajakobsen@gmail.com>
2016-06-30 16:22:26 +0200

    Context values is not a proper set. dub values could occure before.

--- src/main/java/dk/aau/cs/extbi/PFQA/helper/ContextSet.java
+++ src/main/java/dk/aau/cs/extbi/PFQA/helper/ContextSet.java
@@ -1,13 +1,11 @@
 package dk.aau.cs.extbi.PFQA.helper;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.HashSet;
 
 import dk.aau.cs.extbi.PFQA.logger.Logger;
 
 public class ContextSet {
-	List<String> contextValues = new ArrayList<String>();
+	HashSet<String> contextValues = new HashSet<String>();
 
 	public void add(String contextValue) {
 		contextValues.add(contextValue);
@@ -17,39 +15,21 @@ public class ContextSet {
 		contextValues.addAll(contextSet.getValues());
 	}
 	
-	public List<String> getValues() {
+	public HashSet<String> getValues() {
 		return contextValues;
 	}
 
 	public ContextSet intersect(ContextSet contextSetPQ) {
 		Logger logger = Logger.getInstance();
 		
+		
 		logger.startIntersectContextSet();
-		List<String> intersection = new ArrayList<String>();
-		HashMap<String,Boolean> map = new HashMap<String, Boolean>();
-		if (contextValues.size() > contextSetPQ.getValues().size()) {
-			for (String string : contextSetPQ.getValues()) {
-				map.put(string, true);
-			}
-			for (String string : contextValues) {
-				if (map.containsKey(string)) {
-					intersection.add(string);
-				}
-			}
-			
-		} else {
-			for (String string : contextValues) {
-				map.put(string, true);
-			}
-			for (String string : contextSetPQ.getValues()) {
-				if (map.containsKey(string)) {
-					intersection.add(string);
-				}
-			}
-		}
+		HashSet<String> intersection = new HashSet<String>();
+		intersection.addAll(contextValues);
+		intersection.retainAll(contextSetPQ.getValues());
 		
 		contextValues = intersection;
-		logger.endIntersectContextSet();
+		logger.endIntersectContextSet();	
 		
 		return this;
 	}
